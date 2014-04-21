a32af7796b3e7e083f6cc4d899f6584ad5053aa0
Dustin Rice <dustin@pdx.edu>
2014-04-21 14:55:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Repo to contain any and all LDAP utility scripts ##
+
+* **setup_replication.pl** : This is a (fairly) basic script that sets up replication between a list of masters
+                             and a defined replicant. Note that the binddn is hard codede as cn=directory manager.
+                             A typical run of this command would be:
+                             `./setup_replication.pl --replicaname=replicant.oit.pdx.edu --replicapword=thepassword`
---
+++ setup_replication.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/perl -w
+
+# The point of this script is to make it simple to add a new replica to our LDAP system.
+
+ use Net::LDAPS;
+ use strict;
+ use Getopt::Long;
+ use Term::ReadKey;
+
+ my $replicapword;
+ my $replicadn = 'cn=replication manager,cn=config';
+ my $replicaname;
+
+ # Mostly static variables - hence why they aren't changable via command line arguments
+ # You need to bine as directory manager, generally, so ya
+ my $binddn = "cn=directory manager";
+ # Array list of master LDAP servers to create the replication agreement on
+ my @masters = ('owain.oit.pdx.edu');
+
+ GetOptions(
+         'replicadn=s' => \$replicadn,
+         'replicaname=s' => \$replicaname,
+         'replicapword=s' => \$replicapword,
+         'help|?' => sub { &usage(); },
+ );
+
+ if ( !defined($binddn) or 
+      !defined($replicadn) or 
+      !defined($replicaname) or 
+      !defined($replicapword) 
+    ) {
+   print "ERROR: Missing required values!\n";
+   &usage;
+ }
+
+ # Get password from the command line
+ print "Enter password:";
+ ReadMode('noecho'); 
+ chomp(my $bindpass = <STDIN>);
+ ReadMode(0);
+ 
+ # Do a return after accepting pword
+ print "\n";
+
+ # Setup replication dn on replica
+ my $replicaldaps = Net::LDAPS->new($replicaname) or die ("ldap error! $@\n");
+ my $replicamesg = $replicaldaps->bind( $binddn, password => $bindpass);
+
+ my $result = $replicaldaps->add( $replicadn,
+                      attrs => [
+                        'cn'                     => 'replication manager',
+                        'sn'                     => 'RM',
+                        'userpassword'           => $replicapword,
+                        'passwordExpirationTime' => '20380119031407Z',
+                        'nsIdleTimeout'          => '0', 
+                        'objectclass'            => ['top', 'person',
+                                                    'organizationalPerson'],
+                      ]
+                    );
+ $result->code && die ("failed to add entry: $result->error\n");
+
+ $result = $replicaldaps->add( 'cn=replica,cn="dc=pdx,dc=edu",cn=mapping tree,cn=config',
+                      attrs => [
+                        'cn'                     => 'replica',
+                        'nsds5replicaroot'       => 'dc=pdx,dc=edu',
+                        'nsds5replicatype'       => '2',
+                        'nsds5ReplicaBindDN'     => $replicadn,
+                        'nsDS5ReplicaId'         => '65535',
+                        'nsds5flags'             => '0',
+                        'objectclass'            => ['top', 'nsds5replica',
+                                                    'extensibleObject'],
+                      ]
+                    );
+
+ $result->code && die ("failed to add entry: $result->error\n");
+
+ print " Success: Setup DN for replication on $replicaname.\n";
+
+ $replicamesg = $replicaldaps->unbind;
+
+ # Add replication agreement to master
+ foreach my $master ( @masters ) {
+   my $masterldaps = Net::LDAPS->new($master) or die ("ldap error! $@\n");
+   my $mastermesg = $masterldaps->bind( $binddn, password => $bindpass);
+
+   my $result = $masterldaps->add( "cn=$replicaname,cn=replica,cn=\"dc=pdx,dc=edu\",cn=mapping tree,cn=config",
+                      attrs => [
+                        'cn'                         => $replicaname,
+                        'nsds5replicahost'           => $replicaname,
+                        'nsds5replicaport'           => '636',
+                        'nsds5ReplicaBindDN'         => $replicadn,
+                        'nsds5replicabindmethod'     => 'SIMPLE',
+                        'nsds5replicatransportinfo'  => 'SSL',
+                        'nsds5replicaroot'           => 'dc=pdx,dc=edu',
+                        'nsds5replicacredentials'    => $replicapword,
+                        'nsds5BeginReplicaRefresh'   => 'start',
+                        'objectclass'                => ['top',
+                                                         'nsds5replicationagreement'],
+                      ]
+                    );
+
+   $result->code && die ("failed to add entry: $result->error\n");
+
+   print " Success: Setup replication agreement between $master and $replicaname.\n";
+
+   $mastermesg = $masterldaps->unbind;
+ }
+ exit;
+
+sub usage() {
+        print("Usage: setup_replication.pl <options> 
+
+  --host=<host>             Hostname or IP address to connect to.
+
+  --binddn=<binddn>         DN to bind to the LDAP server
+
+  --replicaname=<FQDN>      FQDN of the new replica server
+  --replicapword=<password> Password for the replication manager account
+
+  --help                    Print usage\n\n");
+  exit;
+}
