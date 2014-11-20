541ceb727174af2f6bd5a4cc0207fe092eb9bda3
Wil Cooley <wcooley@nakedape.cc>
2014-11-19 17:20:18 -0800

    Undo dustin's weird single-space indent

--- changelog.pl
+++ changelog.pl
@@ -3,119 +3,114 @@
 # This script is intented to be used in debuging LDAP changes. It queries the changelog
 # and will decode the changes field for easy human consumption
 
- use Net::LDAPS;
- use Getopt::Long;
- use POSIX;
- use Term::ReadKey;
- use strict;
- use warnings;
+use Net::LDAPS;
+use Getopt::Long;
+use POSIX;
+use Term::ReadKey;
+use strict;
+use warnings;
 
- my $uid;
- my $ldapserver;
- my $binddn;
- my $numchanges = 1000;
+my $uid;
+my $ldapserver;
+my $binddn;
+my $numchanges = 1000;
 
- GetOptions(
-         'uid=s'        => \$uid,
-         'binddn=s'     => \$binddn,
-         'changes=s'    => \$numchanges,
-         'host=s'       => \$ldapserver,
-         'help|?'       => sub { &usage(); },
- );
- 
+GetOptions(
+        'uid=s'        => \$uid,
+        'binddn=s'     => \$binddn,
+        'changes=s'    => \$numchanges,
+        'host=s'       => \$ldapserver,
+        'help|?'       => sub { &usage(); },
+);
+
 # These are required parameters
- if ( !defined($uid) or
-      !defined($ldapserver) or
-      !defined($binddn)
-    ) {
-   print "ERROR: Missing required values!\n";
-   &usage;
- }
- 
+if ( !defined($uid) or
+     !defined($ldapserver) or
+     !defined($binddn)
+) {
+    print "ERROR: Missing required values!\n";
+    &usage;
+}
 
- # Get password from the command line
- my $bindpass;
- print "Enter LDAP password: ";
- ReadMode('noecho'); 
- chomp($bindpass = <STDIN>);
- ReadMode(0);
- 
- # Return after we get password
- print "\n";
 
- # Bind to LDAP server
- my $base = "dc=pdx,dc=edu";
- my $ldaps = Net::LDAPS->new($ldapserver) or die ("ldap error! $@\n");
- my $ldapsmesg = $ldaps->bind( $binddn, password => $bindpass) or die ("ERROR: failed to bind $@\n");
+# Get password from the command line
+my $bindpass;
+print "Enter LDAP password: ";
+ReadMode('noecho');
+chomp($bindpass = <STDIN>);
+ReadMode(0);
 
- # Get the lastchangenumber
- my $changenumresult = $ldaps->search ( base    => "",
-                                   scope   => "base",
-                                   filter  => "objectclass=*",
-                                   attrs   =>  ["lastchangenumber"]
-                                  );
+# Return after we get password
+print "\n";
 
- $changenumresult->code && die ($changenumresult->error);
+# Bind to LDAP server
+my $base = "dc=pdx,dc=edu";
+my $ldaps = Net::LDAPS->new($ldapserver) or die ("ldap error! $@\n");
+my $ldapsmesg = $ldaps->bind( $binddn, password => $bindpass) or die ("ERROR: failed to bind $@\n");
 
- my $lastchangenumber= $changenumresult->entry(0)->get_value('lastchangenumber')
-    or die "Unable to find 'lastchangenumber'; is this the LDAP master?\n";
-
- print "Last change: $lastchangenumber\n";
-
- my $oldchangenumber = $lastchangenumber > $numchanges
-                     ? $lastchangenumber - $numchanges
-                     : 0
-                     ;
- 
- # Now lets pull a list of changes
- my $changesresult = $ldaps->search ( base    => "cn=changelog",
-                                      scope   => "sub",
-                                      filter  => "(&(changeNumber>=$oldchangenumber)(targetdn=uid=$uid,ou=people,dc=pdx,dc=edu))",
-                                      attrs   =>  ["changetime","changes"]
+# Get the lastchangenumber
+my $changenumresult = $ldaps->search ( base    => "",
+                                scope   => "base",
+                                filter  => "objectclass=*",
+                                attrs   =>  ["lastchangenumber"]
                                 );
 
- $changesresult->code && die ($changesresult->error);
+$changenumresult->code && die ($changenumresult->error);
 
- my @changes = $changesresult->entries;
+my $lastchangenumber= $changenumresult->entry(0)->get_value('lastchangenumber')
+    or die "Unable to find 'lastchangenumber'; is this the LDAP master?\n";
 
- my $resultcount = $changesresult->count;
+my $oldchangenumber = $lastchangenumber > $numchanges
+                    ? $lastchangenumber - $numchanges
+                    : 0
+                    ;
 
- print "GOT $resultcount changes\n";
+printf "Looking for changes between %d and %d (or so)...\n",
+    $oldchangenumber,
+    $lastchangenumber;
 
- foreach my $change ( @changes ) {
-   my $changeldif = $change->get_value ('changes');
-   my $changetime = $change->get_value ('changetime');
+# Now lets pull a list of changes
+my $changesresult = $ldaps->search ( base    => "cn=changelog",
+                                    scope   => "sub",
+                                    filter  => "(&(changeNumber>=$oldchangenumber)(targetdn=uid=$uid,ou=people,dc=pdx,dc=edu))",
+                                    attrs   =>  ["changetime","changes"]
+                            );
 
-   print "========================================================\n";
-   print "CHANGETIME:$changetime\n";
-   print "-------------------------\n";
-   print "$changeldif\n";
+$changesresult->code && die ($changesresult->error);
 
- }
+my @changes = $changesresult->entries;
 
- exit;
+my $resultcount = $changesresult->count;
+
+print "GOT $resultcount changes\n";
+
+foreach my $change ( @changes ) {
+    my $changeldif = $change->get_value ('changes');
+    my $changetime = $change->get_value ('changetime');
+
+    print "========================================================\n";
+    print "CHANGETIME:$changetime\n";
+    print "-------------------------\n";
+    print "$changeldif\n";
+}
+
+exit;
 
 ## Sub processes
 ############################################
 
 sub usage() {
-        print("Usage: $0 <options> 
+    print("Usage: $0 <options>
 
-  --uid=<user name>                    UID to look at changes for. 
-  
-  --changes=<interger>                 How many changes to look back, defaults to 1000 
+    --uid=<user name>                    UID to look at changes for.
 
-  --binddn=<ldap dn to bind as>        LDAP DN to bind as
-                                       (ex: uid=dustin,ou=people,dc=pdx,dc=edu)
+    --changes=<interger>                 How many changes to look back, defaults to 1000
 
-  --host=<ldap server>                 Server to look at for changelog entries 
-  
-  --help                               Print usage\n\n");
-  exit;
+    --binddn=<ldap dn to bind as>        LDAP DN to bind as
+                                        (ex: uid=dustin,ou=people,dc=pdx,dc=edu)
+
+    --host=<ldap server>                 Server to look at for changelog entries
+
+    --help                               Print usage\n\n");
+    exit;
 }
-
-
-
-
-
-
